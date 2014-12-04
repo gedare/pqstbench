8f9f2183930cc86ed14586356373edbea1932723
Gedare Bloom <gedare@rtems.org>
2014-12-04 15:02:35 -0500

    regenerate params with just one task

--- generators/params.h
+++ generators/params.h
@@ -1,9 +1,4 @@
-/*
- * Copyright 2014 Gedare Bloom (gedare@rtems.org)
- *
- * This file's license is 2-clause BSD as in this distribution's LICENSE.2 file.
- */
-/* command line: -s 256 -d 1 -p 10 -i 100000 -t 4 -a 0.0 -o 1024 -m 1  */
+/* command line: -s 256 -d 1 -p 10 -i 100000 -t 1 -a 0.0 -o 1024 -m 4  */
 /* i: 100000
    alpha: 0.0
    beta: 0.5
@@ -11,13 +6,13 @@
 
 
 
-/* [1, 0, 0, 0] duplicate enqueues during warmup */
+/* [0] duplicate enqueues during warmup */
 
-/* [2, 1, 3, 0] duplicate enqueues during work */
+/* [0] duplicate enqueues during work */
 
-/* The maximum priority value during warmup is [2026392, 1809016, 2339285, 2497165] */
+/* The maximum priority value during warmup is [99787] */
 
-/* The maximum priority value during work is [2552705, 2325984, 2950538, 2939360] */
+/* The maximum priority value during work is [0] */
 
 
 #ifndef __PARAMS_H_
@@ -25,20 +20,17 @@
 
 /* task parameters */
 #define NUM_PERIODIC_TASKS  (0)
-#define NUM_APERIODIC_TASKS (4)
+#define NUM_APERIODIC_TASKS (1)
 #define NUM_TASKS           ( NUM_PERIODIC_TASKS + NUM_APERIODIC_TASKS )
 
 #define build_task_name() do { \
 Task_name[ 1 ] =  rtems_build_name( 'A', 'T', '0', '1' );\
-Task_name[ 2 ] =  rtems_build_name( 'A', 'T', '0', '2' );\
-Task_name[ 3 ] =  rtems_build_name( 'A', 'T', '0', '3' );\
-Task_name[ 4 ] =  rtems_build_name( 'A', 'T', '0', '4' );\
 } while(0)
 
 /* PQ parameters */
 #define PQ_MAX_SIZE  (256)
-extern const int PQ_SIZE[4];
-extern const int PQ_WARMUP_OPS[4];
-extern const int PQ_WORK_OPS[4];
+extern const int PQ_SIZE[1];
+extern const int PQ_WARMUP_OPS[1];
+extern const int PQ_WORK_OPS[1];
 
 #endif
